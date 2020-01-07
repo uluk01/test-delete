53d3f3816d9cd99aa3f9d6bce9eac94dd4d314f7
unknown <ulukbakytbekov@gmail.com>
2020-01-07 20:53:31 +0600

    initial

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".slider").owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true,
+        navText: false
+    });
+});
+$(document).ready(function(){
+    $(".post-slide").owlCarousel({
+        items: 3,
+        margin: 10,
+        loop: true,
+        nav: true,
+        navText: false
+    });
+});
---
+++ readme.md
@@ -0,0 +1,8 @@
+Information about my project
+
+# Header 1
+## Header 2
+
+
+* list 1
+* list 2
